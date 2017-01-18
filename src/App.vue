88d376e7ba609eb75da9c8718f32b549d41c691b
chris <christru@gmail.com>
2017-01-17 23:53:37 -0700

    checkin

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div v-for="u in userIDS">
+        <user :id="u"></user>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import User from './components/User'
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        userIDS: [1, 2, 3, 4, 5,6,7,8,9,10]
+      }
+    },
+    components: {
+      User
+    }
+  }
+
+</script>
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .container{
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <img src="http://librarianinblack.net/librarianinblack/wp-content/uploads/2014/10/Spy.png" alt="">
+    <div class="user">
+      <h1>{{name}}</h1>
+      <p>{{email}}</p>
+    </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    props: ['id'],
+    data: function () {
+      return {
+        name: '',
+        email: ''
+      }
+    },
+    beforeMount() {
+      axios.get('https://jsonplaceholder.typicode.com/users/' + this.id)
+        .then((response) => {
+          this.name = response.data.name
+          this.email = response.data.email
+        })
+    }
+  }
+
+</script>
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  p {
+    margin-top: 0px;
+  }
+  
+  .container{
+    outline: solid;
+    padding: 25px;
+    margin: 5px;
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+  }
+  img{
+    width: 150px;
+  }
+
+</style>
